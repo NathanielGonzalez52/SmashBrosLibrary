47e2cfb89ed1fe840f6f99ee23e2d86b801e577f
Nathan Gonzalez <nathanielgonzalez52@gmail.com--global user.email nathanielgonzalez52@gmail.com>
2022-10-09 17:20:23 -0700

    Add webpages for finding and adding fighter gamertags to library

---
+++ find.js
@@ -0,0 +1,11 @@
+const enter = document.getElementsByClassName("enter")[0];
+
+enter.addEventListener("click", ()=> {
+  // functionality imbound
+})
+
+const back = document.getElementsByClassName("back")[0];
+
+back.addEventListener("click", ()=> {
+  window.location = "welcome.html";
+})
---
+++ welcome.js
@@ -0,0 +1,12 @@
+const find = document.getElementsByClassName("find")[0];
+const add = document.getElementsByClassName("add")[0];
+
+
+add.addEventListener("click", ()=> {
+  window.location = "add.html";
+})
+
+
+find.addEventListener("click", ()=> {
+  window.location = "find.html";
+})
