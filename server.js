703535831b8107b81125845b2ed52da3ad0fbfcd
Nathan Gonzalez <nathanielgonzalez52@gmail.com--global user.email nathanielgonzalez52@gmail.com>
2022-10-12 01:37:55 -0700

    Add node.js express

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const fs = require('browserify-fs');
+// const fs = require('browserify-fs');
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const request = require("request");
+//
+// const app = express();
+//
+// app.listen(3000, function() {
+//   console.log("Server is running on ")
+// })
+
+
+// Barebones express server
 const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
 
 const app = express();
+//What happens at home route
+app.get("/", function(request, response){
+  console.log(request);
+});
 
 app.listen(3000, function() {
-  console.log("Server is running on ")
-})
+  console.log("Server started on port 3000...");
+});
