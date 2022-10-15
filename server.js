81bb662f6b3c564acf9a770485aa5c449333e96a
Nathan Gonzalez <nathanielgonzalez52@gmail.com--global user.email nathanielgonzalez52@gmail.com>
2022-10-15 01:09:21 -0700

    Add images and public folder to store CSS and images for node.js

--- server.js
+++ server.js
@@ -12,13 +12,17 @@
 
 // Barebones express server
 const express = require("express");
+// const request = require("request");
 
 const app = express();
-//What happens at home route
-app.get("/", function(request, response){
-  console.log(request);
-});
+// What happens at home route
 
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/welcome.html");
+});
+//
 app.listen(3000, function() {
   console.log("Server started on port 3000...");
 });
