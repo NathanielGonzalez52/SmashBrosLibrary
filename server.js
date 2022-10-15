ae67c959acc173dad053850400e6d20f2d1aa348
Nathan Gonzalez <nathanielgonzalez52@gmail.com--global user.email nathanielgonzalez52@gmail.com>
2022-10-15 13:30:05 -0700

    Add node.js boilerplate

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
 // const fs = require('browserify-fs');
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const request = require("request");
-//
-// const app = express();
-//
-// app.listen(3000, function() {
-//   console.log("Server is running on ")
-// })
-
-
-// Barebones express server
 const express = require("express");
+const bodyParser = require("body-parser");
 // const request = require("request");
-
+//
 const app = express();
-// What happens at home route
+//
 
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/welcome.html");
 });
-//
+
 app.listen(3000, function() {
-  console.log("Server started on port 3000...");
-});
+  console.log("Server is running on port 3000");
+})
+
+
+const data = {
+  fighter: [
+    {
+      gamerTag: username,
+      fighterOne: fighter1,
+      fighterTwo: fighter2
+  }
+]
+};
+
+// Barebones express server
+// const express = require("express");
+// const request = require("request");
+
+// const app = express();
+// What happens at home route
+
+
+//
+// app.listen(3000, function() {
+//   console.log("Server started on port 3000...");
+// });
